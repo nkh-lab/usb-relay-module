b32ff2bfdf47e8ef50d52ce829e8af9372f552af
Mykola Khyliuk <nkh@ua.fm>
2022-08-03 13:33:20 +0200

    Add project structure

---
+++ src/nrelay.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "utils.h"
+
+int main(int argc, char const *argv[])
+{
+    NOT_USED(argc);
+
+    std::cout << "Hello World from " << argv[0] << "\n";
+
+    /* code */
+    return 0;
+}
---
+++ src/utils.h
@@ -0,0 +1,7 @@
+# pragma once
+
+#define NOT_USED(x) static_cast<void>(x) 
+
+namespace nlab::utils {
+
+}
