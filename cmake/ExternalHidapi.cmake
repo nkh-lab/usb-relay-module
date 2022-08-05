6d965a95854779b1310a91e10b7204a7ad0559bf
Mykola Khyliuk <nkh@ua.fm>
2022-08-05 23:50:10 +0200

    Add Windows to cmake/ExternalHidapi.cmake

--- cmake/ExternalHidapi.cmake
+++ cmake/ExternalHidapi.cmake
@@ -1,11 +1,21 @@
-add_custom_target(
-    external_hidapi
-    COMMAND ./bootstrap
-    COMMAND ./configure
-    COMMAND make
-    WORKING_DIRECTORY ../external/hidapi
-)
+if(CMAKE_SYSTEM_NAME STREQUAL Linux)
+    add_custom_target(
+        external_hidapi
+        COMMAND ./bootstrap
+        COMMAND ./configure
+        COMMAND make
+        WORKING_DIRECTORY ../external/hidapi
+    )
 
-set(HIDAPI_LIB ${CMAKE_SOURCE_DIR}/external/hidapi/linux/.libs/libhidapi-hidraw.so)
-#set(HIDAPI_LIB ${CMAKE_SOURCE_DIR}/external/hidapi/linux/.libs/libhidapi-hidraw.a)
+    set(HIDAPI_LIB ${CMAKE_SOURCE_DIR}/external/hidapi/linux/.libs/libhidapi-hidraw.so)
+    #set(HIDAPI_LIB ${CMAKE_SOURCE_DIR}/external/hidapi/linux/.libs/libhidapi-hidraw.a)
+    
+elseif(CMAKE_SYSTEM_NAME STREQUAL Windows)
+    add_custom_target(
+        external_hidapi
+        COMMAND msbuild hidapi.sln
+        WORKING_DIRECTORY ../external/hidapi/windows
+    )
+endif()
+
 set(HIDAPI_LIB_INCLUDE ${CMAKE_SOURCE_DIR}/external/hidapi/hidapi)
