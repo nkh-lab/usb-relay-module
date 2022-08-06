59f3742fda0daaac906f9db1e4bb1ff8e1585d42
Mykola Khyliuk <nkh@ua.fm>
2022-08-06 02:47:08 +0200

    Bump external/hidapi

--- cmake/ExternalHidapi.cmake
+++ cmake/ExternalHidapi.cmake
@@ -13,8 +13,9 @@ if(CMAKE_SYSTEM_NAME STREQUAL Linux)
 elseif(CMAKE_SYSTEM_NAME STREQUAL Windows)
     add_custom_target(
         external_hidapi
-        COMMAND msbuild hidapi.sln -property:Configuration=Release
-        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/external/hidapi/windows
+        COMMAND dir
+    #    COMMAND msbuild ${CMAKE_SOURCE_DIR}/external/hidapi/windows/hidapi.sln -property:Configuration=Release
+    #    #WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/external/hidapi/windows
     )
 
     set(HIDAPI_LIB ${CMAKE_SOURCE_DIR}/external/hidapi/windows/Release/hidapi.lib)
