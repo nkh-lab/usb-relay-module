5c55d987ceaab34dd4c48a81965ad49058d0214d
Mykola Khyliuk <nkh@ua.fm>
2022-08-08 18:44:58 +0200

    Adapt GetRelayStateTest to Windows

--- tests/component/hidapi/GetRelayStateTest.cpp
+++ tests/component/hidapi/GetRelayStateTest.cpp
@@ -31,10 +31,15 @@ int main(int argc, char const* argv[])
             if (ret != -1)
             {
                 std::cout << BytesToStr(data, sizeof(data)) << "\n";
-
-                std::string board_name(reinterpret_cast<char*>(data), 5);
+#ifdef __linux__
+                std::string board_name(reinterpret_cast<char*>(&data[0]), 5);
+				uint8_t& relays_state = data[7];
+#else
+				std::string board_name(reinterpret_cast<char*>(&data[1]), 5);
+				uint8_t& relays_state = data[8];
+#endif
                 std::cout << "board_name:       " << board_name << "\n";
-                std::cout << "relays_state:     " << std::bitset<8>(data[7]) << "\n";
+                std::cout << "relays_state:     " << std::bitset<8>(relays_state) << "\n";
             }
         }
         else
@@ -48,6 +53,7 @@ int main(int argc, char const* argv[])
 
 /* Example of output:
 
+Ubuntu:
 $ ./GetRelayStateTest
 path:             /dev/hidraw4
 vendor_id:        0x16c0
@@ -60,6 +66,21 @@ interface_number: 0
 ==============================
 hid_get_feature_report(01 00 00 00 00 00 00 00 00) returned 9
 52 31 00 00 00 00 00 01 52
+board_name:       R1
+relays_state:     00000001
+
+Windows:
+path:             \\?\hid#vid_16c0&pid_05df#6&5a5089d&0&0000#{4d1e55b2-f16f-11cf-88cb-001111000030}
+vendor_id:        0x16c0
+product_id:       0x05df
+serial_number:    (null)
+release_number:   256
+manufacturer:     www.dcttech.com
+product:          USBRelay2
+interface_number: -1
+==============================
+hid_get_feature_report(01 00 00 00 00 00 00 00 00) returned 9
+01 52 31 00 00 00 00 00 01
 board_name:       R1
 relays_state:     00000001
 
