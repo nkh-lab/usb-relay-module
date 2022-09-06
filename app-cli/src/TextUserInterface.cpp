500071043309336fcc1e01f41159e2b6f585017a
Mykola Khyliuk <nkh@ua.fm>
2022-09-06 23:28:41 +0200

    Add clang-format off to TextUserInterface.cpp

--- app-cli/src/TextUserInterface.cpp
+++ app-cli/src/TextUserInterface.cpp
@@ -2,14 +2,15 @@
 
 namespace nlab {
 
+// clang-format off
+
 const char* TextUserInterface::kVersion = "v%d.%d.%d\n";
 
 const char* TextUserInterface::kHelp =
     "Usage:\n"
     "    <ModuleName>_<ChannelNumber>    Return a state 0 or 1 of the requested channel 1..8\n"
     "    <ModuleName>                    Return full information about the requested module\n"
-    "    <>                              No arguments provided, return full information about all "
-    "detected modules\n"
+    "    <>                              No arguments provided, return full information about all detected modules\n"
     "\n"
     "Service commands:\n"
     "    -h | --help                     Display this help text\n"
@@ -19,17 +20,23 @@ const char* TextUserInterface::kWrongArgumentUsage =
     "Wrong argument usage!\n"
     "Use the '-h | --help' command to see a description of usage.\n";
 
-const char* TextUserInterface::kBadArgument = "Bad argument '%s' provided!\n";
+const char* TextUserInterface::kBadArgument =
+    "Bad argument '%s' provided!\n";
 
-const char* TextUserInterface::kGetChannelState = "%d\n";
+const char* TextUserInterface::kGetChannelState =
+    "%d\n";
 
 const char* TextUserInterface::kGetRelayInfoAndState =
     "%s\n"
     "module_name:      %s\n"
     "%s\n";
 
-const char* TextUserInterface::kChannelNameAndState = "channel_%d:        %d\n";
+const char* TextUserInterface::kChannelNameAndState =
+    "channel_%d:        %d\n";
 
-const char* TextUserInterface::kNoUsbRelayModule = "No USB relay module detected\n";
+const char* TextUserInterface::kNoUsbRelayModule =
+    "No USB relay module detected\n";
+
+// clang-format on
 
 } // namespace nlab
